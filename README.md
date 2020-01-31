e1d0c55a82f3c8e4bf22fdbf11ecef74ab37931e
Anthony Wilcox <35226681+tonytins@users.noreply.github.com>
2020-01-31 15:31:16 -0500

    Build to most major CI services

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Act 2
 
-![Rust](https://github.com/tonytins/act2/workflows/Rust/badge.svg)
+![Rust](https://github.com/tonytins/act2/workflows/Rust/badge.svg) [![Build Status](https://travis-ci.org/tonytins/act2.svg?branch=master)](https://travis-ci.org/tonytins/act2) [![Build status](https://ci.appveyor.com/api/projects/status/ffru6ik26j2b87ko?svg=true)](https://ci.appveyor.com/project/tonytins/act2) [![codecov](https://codecov.io/gh/tonytins/act2/branch/master/graph/badge.svg)](https://codecov.io/gh/tonytins/act2)
 
 Act 2 is a simple engine for making text-based adventure games using JSON.
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,74 @@
  * This project is licensed under the MIT license.
  * See the LICENSE file in the project root for more information.
  */
+//!
+//! Act 2 is a simple engine for making simple, text-based adventure games.
+//!
+//! # Examples
+//!
+//! ```rust
+//! extern crate act2; // Rust 2015
+//!
+//! use act2::load_game;
+//!
+//! fn main() {
+//!     // Create a string containing our Act game
+//!     let game_string = r#"
+//!     {
+//!        "rooms": [
+//!            {
+//!                "name": "start",
+//!                "scene": "Im a starting room! Welcome to this example game.",
+//!                "actions": [
+//!                    {
+//!                        "variant": "Move",
+//!                        "fields": [
+//!                            "Move to another room","example",""
+//!                        ]
+//!                    }
+//!                ]
+//!            },
+//!            {
+//!                "name": "example",
+//!                "scene": "You enter an example room, with a big, triangular key in it. Theres also a door with a keyhole in triangular shape.",
+//!                "actions": [
+//!                    {
+//!                        "variant": "PickUp",
+//!                        "fields": [
+//!                            "Pick the key up","TriangleKey",""
+//!                        ]
+//!                    },
+//!                    {
+//!                        "variant": "Move",
+//!                        "fields": [
+//!                            "Try to open the door","locked","TriangleKey"
+//!                        ]
+//!                    }
+//!                ]
+//!            },
+//!            {
+//!                "name": "locked",
+//!                "scene": "You picked an item up and used it to open the door! This is the final room. Congratz!",
+//!                "actions": [
+//!                    {
+//!                        "variant": "Move",
+//!                        "fields": [
+//!                            "Move to another room","example",""
+//!                        ]
+//!                    }
+//!                ]
+//!            }
+//!        ]
+//!     }
+//!     "#;
+//!     // Load the game into a proper Game struct
+//!     let mut game = load_game(game_string).unwrap();
+//!     // Start the game
+//!     game.play();
+//!     // Profit!
+//! }
+//! ```
+//!
 pub mod game;
 mod jmodels;
 
