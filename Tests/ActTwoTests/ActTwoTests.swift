86c1610ca450c7093ce7aa9d597d8abe46fba16f
Tony Bark <35226681+tonytins@users.noreply.github.com>
2026-07-22 04:31:41 -0400

    Example game in unit test

--- Tests/ActTwoTests/ActTwoTests.swift
+++ Tests/ActTwoTests/ActTwoTests.swift
@@ -1,8 +1,22 @@
 import Testing
+import Foundation
 @testable import ActTwo
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    // Swift Testing Documentation
-    // https://swiftpackageindex.com/swiftlang/swift-testing/documentation
+let json = """
+{"rooms":[{"name":"start","scene":"Im a starting room! Welcome to this example game.","actions":[{"variant":"Move","fields":["Move to another room","example",""]}]},{"name":"example","scene":"You enter an example room, with a big, triangular key in it. There's also a door with a keyhole in triangular shape.","actions":[{"variant":"PickUp","fields":["Pick the key up","TriangleKey"]},{"variant":"Move","fields":["Try to open the door","locked","TriangleKey"]}]},{"name":"locked","scene":"You picked an item up and used it to open the door! This is the final room. Congratz!","actions":[{"variant":"Move","fields":["Return to start","example"]}]}]}
+"""
+
+func startGame(jsonFile: String, startRoom: String) -> GameEngine? {
+    let jsonData = Data(jsonFile.utf8)
+    let world = try! JSONDecoder().decode(GameWorld.self, from: jsonData)
+    let engine = GameEngine(world: world, startRoomName:startRoom)
+    
+    return engine
+}
+
+@Test func startRoom() async throws {
+
+    let engine = startGame(jsonFile: json, startRoom: "start")
+    
+    #expect(engine?.currentRoom.scene == "Im a starting room! Welcome to this example game.")
 }
